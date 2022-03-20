0b9640db0999bdb4d203c73ae21c347f8dff7bf6
yl554 <yl554@duke.edu>
2022-03-20 12:18:54 -0400

    added feature where once all items are fulfilled, order will be marked as fulfilled

--- mini-amazon-skeleton/app/order.py
+++ mini-amazon-skeleton/app/order.py
@@ -11,13 +11,13 @@ from .models.user import User
 from flask import Blueprint
 bp = Blueprint('order', __name__)
 
-@bp.route('/buyer-order/<uid>', methods=['GET'])
-def orderBuyer(uid):
-    orders = Order.get_by_uid(uid)
+@bp.route('/buyer-order/<bid>', methods=['GET'])
+def orderBuyer(bid):
+    orders = Order.get_by_bid(bid)
     if orders == None:
         return "Error! No such buyer exists!"
     else:
-        return render_template("order.html", orders=orders, uid=uid)
+        return render_template("order.html", orders=orders, bid=bid)
 
 
 @bp.route('/seller-order/<sid>', methods=['GET'])
@@ -29,20 +29,23 @@ def orderSeller(sid):
         return render_template("order_seller.html", orders=orders, sid=sid)
 
 
-class MarkFufilledForm(FlaskForm):
+class ItemFufilledForm(FlaskForm):
     confirm = StringField('Confirm that this product is fulfilled? (Y/N)')
     submit = SubmitField('Submit')
 
 
-@bp.route('/seller/mark-fulfilled/<sid>/<pid>', methods=['GET'])
-def markFulfilled():
-    form = MarkFufilledForm()
+@bp.route('/seller-order/item-fulfilled/<sid>/<oid>/<pid>', methods=['GET','POST'])
+def itemFulfilled(oid, sid, pid):
+    form = ItemFufilledForm()
     confirmation = request.form.get("confirm")
+    #print("confirmation", confirmation)
     if confirmation == 'N':
-        flash('You indicated that this order is NOT fulfilled.')
-        return redirect(url_for("order.orderSeller", sid=sid))
-    if form.validate_on_submit():
-        Order.mark_fulfilled(pid, pid)
-        flash('You indicated that this order is SUCCESSFULLY fulfilled.')
-        return redirect(url_for("order.orderSeller", sid = sid))
-    return render_template('mark_fulfilled.html', title='Mark Fulfilled', form=form, sid=sid, pid=pid)
+        flash('You indicated that this item is NOT fulfilled.')
+        return redirect(url_for("order.orderSeller", sid=1))
+    else:
+        if form.validate_on_submit():
+            Order.item_fulfilled(oid, pid)
+            Order.all_fulfilled_check(oid)
+            flash('You indicated that this item is SUCCESSFULLY fulfilled.')
+            return redirect(url_for("order.orderSeller", sid = sid))
+    return render_template('item_fulfilled.html', title='Item Fulfilled', form=form, sid=sid, oid=oid, pid=pid)
