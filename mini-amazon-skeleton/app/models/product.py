e37ca7a001ea3c39df24ae402c49fb7f20e2566b
yl554 <yl554@duke.edu>
2022-03-27 02:23:50 -0400

    Added basic analytics page

--- mini-amazon-skeleton/app/models/product.py
+++ mini-amazon-skeleton/app/models/product.py
@@ -13,20 +13,20 @@ class Product:
     @staticmethod
     def get(id):
         rows = app.db.execute('''
-SELECT id, name, description, available, category, image_file
-FROM Products
-WHERE id = :id
-''',
+            SELECT id, name, description, available, category, image_file
+            FROM Products
+            WHERE id = :id
+            ''',
                               id=id)
         return Product(*(rows[0])) if rows is not None else None
 
     @staticmethod
     def get_all(available=True):
         rows = app.db.execute('''
-SELECT id, name, description, available, category, image_file
-FROM Products
-WHERE available = :available
-''',
+            SELECT id, name, description, available, category, image_file
+            FROM Products
+            WHERE available = :available
+            ''',
                               available=available)
         return [Product(*row) for row in rows]
 
