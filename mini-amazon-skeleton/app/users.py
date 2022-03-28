bb5b8675230be69e2c81a35555837ac04badaa0d
Hsuan Fu Liu <hl325@duke.edu>
2022-03-28 12:26:09 -0400

    change hardcoded user id in history page

--- mini-amazon-skeleton/app/users.py
+++ mini-amazon-skeleton/app/users.py
@@ -178,10 +178,9 @@ def purchase_history():
     if current_user.is_authenticated:
         
         if request.method == "GET":
-            # purchases = Purchase.get_all_by_uid_since(current_user.id, datetime.datetime(1980, 9, 14, 0, 0, 0))
             ancient = datetime.datetime(1980, 9, 14, 0, 0, 0)
             now = datetime.datetime.now()
-            purchases = Purchase.get_all_by_uid_since(0, ancient, now)
+            purchases = Purchase.get_all_by_uid_since(current_user.id, ancient, now)
             potential_sellers = list(set([ p.sname for p in purchases ]))
             potential_items = list(set([ p.product for p in purchases ]))
             
@@ -223,7 +222,7 @@ def purchase_history():
                 seller_firstname = '%' + input_seller[0].lower() + '%'
             
 
-            purchases = Purchase.get_all_by_uid_since(0, datetime_start, datetime_end, product, seller_firstname, seller_lastname)
+            purchases = Purchase.get_all_by_uid_since(current_user.id, datetime_start, datetime_end, product, seller_firstname, seller_lastname)
             potential_sellers = list(set([ p.sname for p in purchases ]))
             potential_items = list(set([ p.product for p in purchases ]))
             
